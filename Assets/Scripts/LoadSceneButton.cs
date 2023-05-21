b09b0392f85067e28813d34e78c70e07554d6979
Pukitis <73535419+Rumfur@users.noreply.github.com>
2023-05-21 07:29:57 +0300

    Fixed Model Height

---
+++ Assets/Scripts/LoadSceneButton.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace KartGame.UI
+{
+    public class LoadSceneButton : MonoBehaviour
+    {
+        [Tooltip("What is the name of the scene we want to load when clicking the button?")]
+
+        	public void LoadScene()
+    	{
+        	SceneManager.LoadScene("level2B");
+    	}
+    }
+}
